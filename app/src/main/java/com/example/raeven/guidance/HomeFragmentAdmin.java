50851d05d688e3c67d1f38a89141c13f2a6a6e05
iron <ironmaturan7@gmail.com>
2016-11-26 11:22:30 +0800

    Commit

--- app/src/main/java/com/example/raeven/guidance/HomeFragmentAdmin.java
+++ app/src/main/java/com/example/raeven/guidance/HomeFragmentAdmin.java
@@ -5,6 +5,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,11 +15,16 @@ import android.widget.LinearLayout;
 import android.widget.ScrollView;
 import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 
 /**
@@ -54,7 +60,25 @@ public class HomeFragmentAdmin extends Fragment implements View.OnClickListener{
         b.setOnClickListener(this);
         ll.addView(b);
 
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                Log.e("Count ","" + dataSnapshot.getChildrenCount());
+                ArrayList<String> listahan = new ArrayList<>();
 
+                for(DataSnapshot dsp: dataSnapshot.getChildren()){
+                    listahan.add(String.valueOf(dsp.getValue()));
+
+                }
+
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
         return v;
     }
 
